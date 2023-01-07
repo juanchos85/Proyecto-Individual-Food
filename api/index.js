f41a164565d5b6625c66f774a74309b609ac76e6
Juan Bosque <86373520+juanchos85@users.noreply.github.com>
2023-01-07 20:40:44 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const DB_PORT = process.env.DB_PORT || 3001;
+const DB_PORT = process.env.DB_PORT || 3045;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
