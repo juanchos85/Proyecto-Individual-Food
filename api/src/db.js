fd8f2ea0b3753db63bf0228cd6e2d13b747d9fb4
Juan Bosque <86373520+juanchos85@users.noreply.github.com>
2023-01-07 21:18:31 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -10,6 +10,15 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
+sequelize
+        .authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch((err) => {
+            console.log('Unable to connect to the database:', err);
+        });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
