39f0ed2fe212cb735266de6c439a554b6dae108a
Juan Bosque <86373520+juanchos85@users.noreply.github.com>
2023-01-10 09:20:00 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -13,7 +13,7 @@ let sequelize =
         database: DB_NAME,
         dialect: "postgres",
         host: DB_HOST,
-        port: 5432,
+        port: DB_PORT,
         username: DB_USER,
         password: DB_PASSWORD,
         pool: {
@@ -31,7 +31,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
         { logging: false, native: false }
       );
 const basename = path.basename(__filename);
